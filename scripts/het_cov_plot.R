93c2165d0c54082565054bb5cecc0953de7f245b
azwadriqbal <ari22@cornell.edu>
2023-04-18 12:26:31 -0400

    initial commit

---
+++ scripts/het_cov_plot.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(cowplot)
+
+het_cov_plot <- plot_grid(
+  snp_window_plot,
+  genome_cov_plot,
+  ncol = 1,
+  align = "v"
+)
---
+++ scripts/minimap.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+# minimap script
+
+#1:sr/map-pb/map-ont
+#2:path/to/assembly
+#3:output name
+#4:input fastq files
+
+minimap2 -ax $1 -t 16 $2 ${@:4} > $3.sam
+
+samtools sort -@16 -O BAM -o $3.bam  $3.sam
