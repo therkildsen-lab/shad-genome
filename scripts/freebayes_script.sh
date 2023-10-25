c011a939c83332de8a76c9df6dcb6d32396c1943
azwadriqbal <ari22@cornell.edu>
2023-10-25 16:05:45 -0400

    updated synteny plot

--- scripts/freebayes_script.sh
+++ scripts/freebayes_script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-INPUT_BAM=$1
-THREADS=$2
+BAMLIST=$1
+REF=$2
+THREADS=$3
+OUTDIR=$4
+POPNAME=$5clear
 
-freebayes-parallel <(fasta_generate_regions.py ../assembly/Alosa_sapidissima_fAloSap1.pri_genome.fna.fai 100000) $THREADS -f ../assembly/Alosa_sapidissima_fAloSap1.pri_genome.fna $INPUT_BAM | vcffilter -f "QUAL > 20" > freebayes_results.vcf
+freebayes-parallel <(fasta_generate_regions.py $REF 100000) $THREADS -f $REF -L $BAMLIST | vcffilter -f "QUAL > 20" | bgzip > $OUTDIR'/'$POPNAME'_freebayes_results.vcf.gz'
 
-gzip freebayes_results.vcf
-
-#freebayes -f /workdir/azwad/shad_genome/genomes/fAloSap1/assembly/Alosa_sapidissima_fAloSap1.pri_genome.fna fAloSap1_mapped.sorted.bam | 
+#freebayes -f /workdir/azwad/shad_genome/genomes/fAloSap1/assembly/Alosa_sapidissima_fAloSap1.pri_genome.fna fAloSap1_mapped.sorted.bam |
 #vcffilter -f "QUAL > 20" > results.vcf
